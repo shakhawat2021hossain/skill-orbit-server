d4bf4c12d311bf22b7b35eea6a223b2bdc946209
Shakhawat Hossain <shakhawat2021hossain2021@gmail.com>
2025-12-23 20:41:50 +0600

    updated cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -18,12 +18,12 @@ const register = catchAsync(async (req: Request, res: Response) => {
 const credentialLogin = catchAsync(async (req: Request, res: Response) => {
     const result = await authServices.credentialLogin(req.body)
 
-    res.cookie("accessToken", result.accessToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    })
+    // res.cookie("accessToken", result.accessToken, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: "none",
+    //     maxAge: 7 * 24 * 60 * 60 * 1000
+    // })
 
     sendResponse(res, {
         data: result,
